bf1bad1713b1d63f649c5822811f4a5375ee5ee3
iqbal-sk <iqbal@Iqbals-MacBook-Pro.local>
2024-07-13 19:25:00 +0530

    Configuration for data ingestion from europarl dataset is done

---
+++ src/LangWizAI/entity/config_entity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class EuroparlDataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    unzip_dir: Path
+    dataset_names: list
